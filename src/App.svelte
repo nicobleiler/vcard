7726bc5de21b142998a9b771ad5f205dd09bf870
Nico Bleiler <nico.bleiler@gmail.com>
2021-07-17 20:15:53 +0200

    Updated metadata and added animated scroll icon

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import Name from './Name.svelte';
+	import ScrollIcon from './ScrollIcon.svelte';
 	let name: string = "Nico Bleiler";
-	let attributes: Array<string> = ["a developer.", "an engineer.", "Batman", "a tinkerer.", "a gamer.", "running out of attributes to put here.", "Ironman."]
+	let attributes: Array<string> = ["a developer.", "an engineer.", "Batman", "a tinkerer.", "a gamer.", "running out of ideas to put here.", "Ironman.", "looking for a Job"]
 </script>
 
 <main>
 	<header class="header center">
-		<Name name={name} attributes={attributes}></Name>
+		<div id="info">
+			<Name name={name} attributes={attributes}></Name>
+		</div>
+		<div id="scroll">
+			<ScrollIcon></ScrollIcon>
+		</div>
 	</header>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, provident consectetur inventore, magni veritatis tempora nulla officiis recusandae beatae non laudantium, alias commodi veniam sit! Rerum nobis veritatis similique eveniet?</p>
 </main>
@@ -15,12 +21,20 @@
 	.header {
 		width: 100%;
 		height: 100vh;
-		background-image: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4) ), url("/images/background-code.jpg");	
+		background-image: linear-gradient(rgba(0, 0, 0, .9), rgba(0, 0, 0, 0.7) ), url("/images/background-code.jpg");	
+  		background-size: cover;
+  		background-repeat: no-repeat;
 	}
 	.center {
 		text-align: center;
         display: flex;
         flex-flow: column;
         justify-content: center;
+	}
+	#scroll {
+		position: absolute;
+		display: block;
+		top: 90%;
+		left: 50%;
 	}
 </style>
